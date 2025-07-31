6bab2ca2b5adc0ce514cf76b00b487fc7e3a1a79
Karishma <karishmaelsa23march@gmail.com>
2025-07-30 17:40:46 -0700

    modify APIs to use The database instead of lists to store data

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ import models
 def create_app(db_url=None):
     app = Flask(__name__)
 
-    app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Store REST API"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
@@ -19,17 +18,16 @@ def create_app(db_url=None):
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
-    app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
 
     api = Api(app)
 
-    @app.before_first_request
-    def create_tables():
+    with app.app_context():
         db.create_all()
-        
+
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
 
     return app
-
--- resources/item.py
+++ resources/item.py
@@ -2,6 +2,9 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from db import db
+from sqlalchemy.exc import SQLAlchemyError
+from models import ItemModel
 from schemas import ItemSchema, ItemUpdateSchema
 
 blp = Blueprint("items", __name__, description="Operations on items")
@@ -11,42 +14,52 @@ class Item(MethodView):
     @blp.response(200, ItemSchema)
     def get(self, item_id):   
         try:
-            return items[item_id]   
-        except KeyError:
-            abort(404, message="Item not found.")
+            item = ItemModel.query.get_or_404(item_id)
+            return item
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while fetching the item.")
 
     def delete(self, item_id):
         try:
-            del items[item_id]
-            return {"message": "Item deleted."}
-        except KeyError:
-            abort(404, message="Item not found.")
+            item = ItemModel.query.get_or_404(item_id)
+            db.session.delete(item)
+            db.session.commit()
+            return {"message": "Item deleted"}
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while deleting the item.")
 
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
         try:
-            item = items[item_id]
-            item |= item_data
+            item = ItemModel.query.get(item_id)
+            if item:
+                item.price = item_data["price"] 
+                item.name = item_data["name"]
+            else:
+                item = ItemModel(id = item_id, **item_data)
+            db.session.add(item)
+            db.session.commit()
             return item
-        except KeyError:
-            abort(404, message="Item not found.")
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while updating the item.")
 
 @blp.route("/item")
 class ItemList(MethodView):
     @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return items.values()
+        try:
+            return ItemModel.query.all()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while fetching items.")
     
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, request_data):
-
-        for item in items.values():
-            if item["name"] == request_data["name"] and item["store_id"] == request_data["store_id"]:
-                abort(400, message="Item already exists in this store.")
-
-        item_id = uuid.uuid4().hex
-        item = {**request_data, "id": item_id}
-        items[item_id] = item
-        return item
+        try:
+            item = ItemModel(**request_data)
+            db.session.add(item)
+            db.session.commit()
+            return item
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the item.")
